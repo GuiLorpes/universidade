e2b196a9e1c800f1131cae2baf8d2d80f87b4423
Guilherme Lopes <156134248+GuiLorpes@users.noreply.github.com>
2026-05-07 16:22:08 -0300

    Comecei o trabalho :(

---
+++ semestre_03/PS/listas/Trabalho/calculoMatrizes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Verifica o tamanho da matriz
+//     Se a matriz for:
+//         >= 4 calcula por Laplace
+//         == 3 calcula por Sarrus
+//         == 2 calcula por determinante 2x2
+//         == 1 retorna ele mesmo
+int calculaMatriz(matriz) {
+    
+}
+
+
+// Verifica qual a linha/coluna com mais zeros
+// A partir dela, faz os calculos da soma dos cofatores
+//     Para os cofatores, calcula a matriz resultande da exclusão da linha ij
+int calculaLaplace(matriz) {
+
+}
+
+
+// Multiplica os resultados
+int calculaSarrus() {
+
+}
+
+// Recebe uma matriz para ser calculada
+void recebeMatrizes(ponteiro){
+
+} 
+
+void main(void) {
+    matriz = recebeMatrizes(matriz);
+    printf(calculaMatriz(matriz));
+}
+ 
