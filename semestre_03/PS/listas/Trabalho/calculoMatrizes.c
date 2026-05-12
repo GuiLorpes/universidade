3cd291f18f884785bfb8ce25c1960681a97bbfea
Guilherme Lopes <156134248+GuiLorpes@users.noreply.github.com>
2026-05-12 16:50:22 -0300

    comecei com a função de receber a matriz mas ainda nn funciona

--- semestre_03/PS/listas/Trabalho/calculoMatrizes.c
+++ semestre_03/PS/listas/Trabalho/calculoMatrizes.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Verifica o tamanho da matriz
-//     Se a matriz for:
-//         >= 4 calcula por Laplace
-//         == 3 calcula por Sarrus
-//         == 2 calcula por determinante 2x2
-//         == 1 retorna ele mesmo
-int calculaMatriz(matriz) {
+// // Verifica o tamanho da matriz
+// //     Se a matriz for:
+// //         >= 4 calcula por Laplace
+// //         == 3 calcula por Sarrus
+// //         == 2 calcula por determinante 2x2
+// //         == 1 retorna ele mesmo
+// int calculaMatriz(matriz) {
     
-}
+// }
 
 
-// Verifica qual a linha/coluna com mais zeros
-// A partir dela, faz os calculos da soma dos cofatores
-//     Para os cofatores, calcula a matriz resultande da exclusão da linha ij
-int calculaLaplace(matriz) {
+// // Verifica qual a linha/coluna com mais zeros
+// // A partir dela, faz os calculos da soma dos cofatores
+// //     Para os cofatores, calcula a matriz resultande da exclusão da linha ij
+// int calculaLaplace(matriz) {
 
-}
+// }
 
 
-// Multiplica os resultados
-int calculaSarrus() {
+// // Multiplica os resultados
+// int calculaSarrus() {
 
-}
+// }
 
 // Recebe uma matriz para ser calculada
-void recebeMatrizes(ponteiro){
-
+void recebeMatrizes(float **matriz){
+    int ordem;
+    printf("Insira qual a ordem da sua matriz:\n");
+    scanf("%i", &ordem);
+    *matriz = malloc(ordem * sizeof(float));
+    for (int i = 0; i < ordem; i++) {
+        matriz[i] = malloc(ordem * sizeof(float));
+        for (int j = 0; j < ordem; j++) {
+            printf("Insira o elemento a%i%i da matriz:\n", i, j);
+            matriz[i][j] = scanf("%f\n");
+        }
+    }
+    for (int i = 0; i < ordem; i++) {
+        for (int j = 0; j < ordem; j++) {
+            printf("%f ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 } 
 
 void main(void) {
-    matriz = recebeMatrizes(matriz);
-    printf(calculaMatriz(matriz));
+    float *matriz = NULL; 
+    recebeMatrizes(&matriz);
+    // printf(calculaMatriz(matriz));
 }
  
