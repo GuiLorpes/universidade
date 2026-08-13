8e102d1d22683d3889121ac9f10fdeac8aa7dd38
Guilherme Lopes <guilopes461@gmail.com>
2026-08-13 10:08:43 -0300

    coisinhas que esqueci de adicionar do semestre passado

--- semestre_03/POO/listas/trabalhos/jogoDaVelhaCRUD/jogoDaVelhaCRUD/src/main/java/com/trabalho/jogoDaVelhaCRUD/Model/Jogador.java
+++ semestre_03/POO/listas/trabalhos/jogoDaVelhaCRUD/jogoDaVelhaCRUD/src/main/java/com/trabalho/jogoDaVelhaCRUD/Model/Jogador.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Jogador {
